03cfda6f366bedb75ef382495a7a1eb41ef424e6
shada elkhoja <s.elkhoja@techlines.ly>
2019-12-16 22:58:03 +0200

    home page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,7 +1,7 @@
 <html>
     <head>
       <style>
-      #carouselExampleControls{ height:500; width:1366 !important} 
+      #carouselExampleControls{ height:500; width:1113 !important} 
       </style>
       <title>Blood Bank</title>
       <link rel="stylesheet" href="bootstrap.min.css">
@@ -34,28 +34,7 @@
   
     </nav>
     
-    <div id="carouselExampleControls" class="carousel slide " data-ride="carousel">
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img class="d-block w-100 h-100 " src="slide1.jpg" alt="First slide">
-        
-      </div>
-      <div class="carousel-item">
-        <img class="d-block  w-100 h-100 " src="cart.jpg" alt="Second slide">
-      </div>
-      <div class="carousel-item">
-        <img class="d-block w-100 h-100" src="blooddonor.jpg" alt="Third slide">
-      </div>
-    </div>
-    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="sr-only">Next</span>
-    </a>
-  </div>
+    
   <div class="container">
     @yield('content')
   </div>
